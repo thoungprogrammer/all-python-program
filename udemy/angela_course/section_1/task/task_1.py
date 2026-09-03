e94d7d0dad3917a30b4cac60186570a38d41f173
Morn Chanthoung <thoungprogrammer@gmail.com>
2026-09-04 00:08:47 +0700

    solve the task 1 in section 1

---
+++ udemy/angela_course/section_1/task/task_1.py
@@ -0,0 +1,5 @@
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of salt.")
+print("4. Leave the rise for 2 hours.")
+print("5. Bake at 200 degree C for 30 minutes.")
